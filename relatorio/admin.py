abca66d36caaae8ad6f6969b7b4591c9085a1845
Camila Freitas <camilafreitas.economia@gmail.com>
2023-07-28 18:42:54 -0300

    Carga inicial

--- relatorio/admin.py
+++ relatorio/admin.py
@@ -1,10 +1,10 @@
-# from django.contrib import admin
-# from .models import DiscadorOcorrencia, Sistema, Carteira, Ocorrencia
-#
-# admin.site.register(DiscadorOcorrencia)
-# admin.site.register(Sistema)
-# admin.site.register(Carteira)
-# admin.site.register(Ocorrencia)
+from django.contrib import admin
+from .models import DiscadorOcorrencia, Sistema, Carteira, Ocorrencia
+
+admin.site.register(DiscadorOcorrencia)
+admin.site.register(Sistema)
+admin.site.register(Carteira)
+admin.site.register(Ocorrencia)
 
 
 
