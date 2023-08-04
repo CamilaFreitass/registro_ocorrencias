aa8aad08f9aee1163b9842dc0e66ce0179577df0
Camila Freitas <camilafreitas.economia@gmail.com>
2023-08-04 11:43:14 -0300

    configurando pastas 'media' e 'static'

--- formulario/settings.py
+++ formulario/settings.py
@@ -128,6 +128,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'formulario/static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
